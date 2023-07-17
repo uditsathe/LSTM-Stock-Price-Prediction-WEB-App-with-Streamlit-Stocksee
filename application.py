63eb19c0499e76a947fcc55daf4e893ec4201e0f
uditsathe <102481732+uditsathe@users.noreply.github.com>
2023-07-17 12:01:29 +0530

    Update application.py

--- application.py
+++ application.py
@@ -42,7 +42,7 @@ ma100 = data.Close.rolling(100).mean()
 ma200 = data.Close.rolling(200).mean()
 
 #now plotting 100 day moving average
-fig100ma , bx= plt.subplots(figsize=(14, 8))
+fig100ma , bx= plt.subplots()
 bx.plot(ma100.tail(300), color='blue', label='100 DMA')
 bx.plot(data.tail(300).Close, color='black', label='Close')
 bx.legend(loc='upper left')
